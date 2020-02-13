746a99112d5ef896e80710ba0ac99358205fe5ff
parents <parents@mac-mini-de-mini.home>
2020-02-13 22:08:51 +0100

    version en clair
    Signed-off-by: parents <parents@mac-mini-de-mini.home>

--- rencontres/catalogue.js
+++ rencontres/catalogue.js
@@ -27,81 +27,6 @@ var scenario = [
     [
         {
             id: 2,
-            rencontre: "Mouzillon/Les Herbiers",
-            poster: "EMouz_Herbiers.png",
-            fichier: "EMouz_Herbiers.mp4",
-            scenario: "EMouz_Herbiers.js",
-            description: "Match amical opposant Mouzillon aux Herbiers",
-            gauche: {
-                nom: "Les Herbiers",
-                fanion: "LHerbiers.png",
-                site: "https://www.vendeelesherbiersfootball.fr/pages/index.php",
-                maillotCouleur: "rgb(120, 83, 52)"
-            },
-            droite: {
-                nom: "Etoile Mouzillon",
-                fanion: "EMouz.png",
-                site: "https://etoile-mouzillon.footeo.com",
-                maillotCouleur: "rgb(255, 255, 255)"
-            },
-            arbitre: {
-                maillotCouleur: "rgb(94, 206, 226)"
-            }
-        }
-    ],
-    [
-        {
-            id: 3,
-            rencontre: "Mouzillon/Baugé",
-            poster: "EMouz_Bauge.png",
-            fichier: "EMouz_Bauge.mp4",
-            scenario: "EMouz_Bauge.js",
-            description: "Match amical opposant Mouzillon et Baugé",
-            gauche: {
-                nom: "Baugé",
-                fanion: "Bauge.png",
-                site: "https://lfpl.fff.fr",
-                maillotCouleur: "rgb(255, 128, 0)"
-            },
-            droite: {
-                nom: "Mouzillon",
-                fanion: "EMouz.png",
-                site: "https://footbretagne.fff.fr",
-                maillotCouleur: "rgb(255, 255, 255)"
-            },
-            arbitre: {
-                maillotCouleur: "rgb(137, 126, 116)"
-            }
-        }
-    ],
-    [
-        {
-            id: 4,
-            rencontre: "CBAF / Metz",
-            poster: "CBAF_Metz.png",
-            fichier: "CBAF_Metz.mp4",
-            scenario: "CBAF_Metz.js",
-            description: "Match u19Nat entre Croix-Blanche et FC Metz",
-            gauche: {
-                nom: "Croix-Blanche",
-                fanion: "cbaf.png",
-                site: "https://www.enavantbaugeois.net",
-                maillotCouleur: "rgb(255, 255, 255)"
-            },
-            droite: {
-                nom: "FC Metz",
-                fanion: "fc_metz.png",
-                site: "https://etoile-mouzillon.footeo.com",
-                maillotCouleur: "rgb(107, 4, 4)"
-            },
-            arbitre: {
-                maillotCouleur: "rgb(156, 170, 56)"
-            }
-        }
-    ],
-    [
-        {
-            id: 5,
             rencontre: "Bretagne/PDLL",
             poster: "Pole_PloufraganMT2.png",
             fichier: "Pole_PloufraganMT2.mp4",
@@ -118,31 +43,6 @@ var scenario = [
                 fanion: "ligueBretagne.png",
                 site: "https://footbretagne.fff.fr",
                 maillotCouleur: "rgb(255, 255, 255)"
-            },
-            arbitre: {
-                maillotCouleur: "rgb(255, 112, 119)"
-            }
-        }
-    ],
-    [
-        {
-            id: 6,
-            rencontre: "FC Lorient/PDLL",
-            poster: "Pole_FCLorient.png",
-            fichier: "Pole_FCLorient.mp4",
-            scenario: "Pole_FCLorient.js",
-            description: "Match amical opposant Le Pôle espoir des Ligues de Bretagne et le FC Lorient (2ème mi-temps)",
-            gauche: {
-                nom: "Ligue Pays De la Loire",
-                fanion: "Ligue2.png",
-                site: "https://lfpl.fff.fr",
-                maillotCouleur: "rgb(67, 76, 108)"
-            },
-            droite: {
-                nom: "FC Lorient",
-                fanion: "FCLorient.png",
-                site: "https://fclweb.fr/",
-                maillotCouleur: "rgb(245, 129, 19)"
             },
             arbitre: {
                 maillotCouleur: "rgb(255, 112, 119)"
