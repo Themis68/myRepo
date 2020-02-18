463c541f783c2c6adc3d97333ecc3277c8b1507a
parents <parents@mac-mini-de-mini.home>
2020-02-18 22:53:40 +0100

    version quizz définitive
    Signed-off-by: parents <parents@mac-mini-de-mini.home>

--- questionnaires/catalogue.js
+++ questionnaires/catalogue.js
@@ -1,14 +1,24 @@
 var scenario = [
-    [
         {
             id: 1,
-            titre: "Les corners",
-            poster: "corner.png",
+            titre: "Les joueurs",
+            poster: "joueurs.png",
             fichier: "bases_01.min.js",
-            description: "Questionnaire sur les bases du corner",
-            loi: "17",
+            variable: "script1",
+            description: "Questionnaire sur les joueurs",
+            loi: "03",
             niveau: 1,
-            temps: "00:01:00"
+            temps: 5
+        },
+        {
+            id: 2,
+            titre: "Les joueurs",
+            poster: "joueurs.png",
+            fichier: "bases_01.min.js",
+            variable: "script1",
+            description: "Questionnaire sur les joueurs",
+            loi: "03",
+            niveau: 2,
+            temps: 5
         }
-    ]
 ]
