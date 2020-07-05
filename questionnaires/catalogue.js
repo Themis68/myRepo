aa7f8e75d2741260f1bc45960e62155b0a8b7cb1
parents <parents@mac-mini-de-mini.home>
2020-07-05 14:34:45 +0200

    corrections diverses sur taille fenetre
    Signed-off-by: parents <parents@mac-mini-de-mini.home>

--- questionnaires/catalogue.js
+++ questionnaires/catalogue.js
@@ -1,31 +1,60 @@
 var scenario = [
-        {
-            titre: "Les joueurs",
-            poster: "joueurs.png",
-            fichier: "bases_01.min.js",
-            variable: "script1",
-            description: "Questionnaire sur les joueurs",
-            loi: 3,
-            niveau: 1,
-            temps: 10
-        },
-        {
-            titre: "Les joueurs",
-            poster: "joueurs.png",
-            fichier: "bases_01.min.js",
-            variable: "script1",
-            description: "Questionnaire sur les joueurs",
-            loi: 3,
-            niveau: 2,
-            temps: 10
-        },
-        {
-            titre: "Les bases",
-            poster: "bases.png",
-            fichier: "bases_02.min.js",
-            variable: "script2",
-            description: "Questionnaire sur les bases de l'arbitrage",
-            niveau: 1,
-            temps: 10
-        }
+    {
+        titre: "Les joueurs",
+        poster: "joueurs.png",
+        fichier: "bases_01.min.js",
+        variable: "script1",
+        description: "Questionnaire sur les joueurs",
+        loi: 3,
+        niveau: 1,
+        temps: 10
+    },
+    {
+        titre: "Les joueurs",
+        poster: "joueurs.png",
+        fichier: "bases_01.min.js",
+        variable: "script1",
+        description: "Questionnaire sur les joueurs",
+        loi: 3,
+        niveau: 2,
+        temps: 10
+    },
+    {
+        titre: "Les bases",
+        poster: "bases.png",
+        fichier: "bases_02.min.js",
+        variable: "script2",
+        description: "Questionnaire sur les bases de l'arbitrage",
+        niveau: 1,
+        temps: 10
+    },
+    {
+        titre: "Les joueurs",
+        poster: "joueurs.png",
+        fichier: "bases_01.min.js",
+        variable: "script1",
+        description: "Questionnaire sur les joueurs",
+        loi: 3,
+        niveau: 1,
+        temps: 10
+    },
+    {
+        titre: "Les joueurs",
+        poster: "joueurs.png",
+        fichier: "bases_01.min.js",
+        variable: "script1",
+        description: "Questionnaire sur les joueurs",
+        loi: 3,
+        niveau: 2,
+        temps: 10
+    },
+    {
+        titre: "Les bases",
+        poster: "bases.png",
+        fichier: "bases_02.min.js",
+        variable: "script2",
+        description: "Questionnaire sur les bases de l'arbitrage",
+        niveau: 1,
+        temps: 10
+    }
 ]
--- src/quizz.js
+++ src/quizz.js
@@ -9,23 +9,23 @@ var stepBarre = 0;			// % de progression pour une Question
 var stepDone = 0;			// % de progression effectué
 var idQuizz = null;		// quizz en cours
 var lois = [
-	{num:1, libelle:"Terrain", fichier="loi_01.pdf"},
-	{num:2, libelle:"Ballon", fichier="loi_02.pdf"},
-	{num:3, libelle:"Joueurs", fichier="loi_03.pdf"},
-	{num:4, libelle:"Equipements", fichier="loi_04.pdf"},
-	{num:5, libelle:"Arbitre", fichier="loi_05.pdf"},
-	{num:6, libelle:"Autres arbitres", fichier="loi_06.pdf"},
-	{num:7, libelle:"Durée d'un match", fichier="loi_07.pdf"},
-	{num:8, libelle:"Coup d'envoi et reprise de jeu", fichier="loi_08.pdf"},
-	{num:9, libelle:"Ballon en jeu et hors du jeu", fichier="loi_09.pdf"},
-	{num:10, libelle:"Issue d'un match", fichier="loi_10.pdf"},
-	{num:11, libelle:"Hors-jeu", fichier="loi_11.pdf"},
-	{num:12, libelle:"Fautes et incorrections", fichier="loi_12.pdf"},
-	{num:13, libelle:"Coups francs", fichier="loi_13.pdf"},
-	{num:14, libelle:"Penalty", fichier="loi_14.pdf"},
-	{num:15, libelle:"Rentrée de touche", fichier="loi_15.pdf"},
-	{num:16, libelle:"Coup de pied de but", fichier="loi_16.pdf"},
-	{num:17, libelle:"Corner", fichier="loi_17.pdf"}
+	{num:1, libelle:"Terrain", fichier:"loi_01.pdf"},
+	{num:2, libelle:"Ballon", fichier:"loi_02.pdf"},
+	{num:3, libelle:"Joueurs", fichier:"loi_03.pdf"},
+	{num:4, libelle:"Equipements", fichier:"loi_04.pdf"},
+	{num:5, libelle:"Arbitre", fichier:"loi_05.pdf"},
+	{num:6, libelle:"Autres arbitres", fichier:"loi_06.pdf"},
+	{num:7, libelle:"Durée d'un match", fichier:"loi_07.pdf"},
+	{num:8, libelle:"Coup d'envoi et reprise de jeu", fichier:"loi_08.pdf"},
+	{num:9, libelle:"Ballon en jeu et hors du jeu", fichier:"loi_09.pdf"},
+	{num:10, libelle:"Issue d'un match", fichier:"loi_10.pdf"},
+	{num:11, libelle:"Hors-jeu", fichier:"loi_11.pdf"},
+	{num:12, libelle:"Fautes et incorrections", fichier:"loi_12.pdf"},
+	{num:13, libelle:"Coups francs", fichier:"loi_13.pdf"},
+	{num:14, libelle:"Penalty", fichier:"loi_14.pdf"},
+	{num:15, libelle:"Rentrée de touche", fichier:"loi_15.pdf"},
+	{num:16, libelle:"Coup de pied de but", fichier:"loi_16.pdf"},
+	{num:17, libelle:"Corner", fichier:"loi_17.pdf"}
 ];
 
 /*var cardIcones = [
@@ -63,6 +63,7 @@ var pathBadges = pathQuizz + "badges/";		//  badges
 // **********************************************************************************************************
 
 document.addEventListener("DOMContentLoaded", init, false);	// lance l'écoute des évènements et appelle INIT
+//window.MediaQueryList.addListener(lookScreen);
 
 function init() {
     //
@@ -75,6 +76,10 @@ function init() {
 	var bascule = document.querySelector("bascule img");
 	bascule.addEventListener("click", fBascule);	// de haut en bas
 	creerVignettes("vignettes");					        // générer le vignettes dans le carousel
+}
+
+function lookScreen(evt) {
+	console.log(evt);
 }
 
 function creerVignettes(id) {
@@ -99,7 +104,8 @@ function creerVignettes(id) {
 
 		// div
 		let myDiv = document.createElement("div");
-		myDiv.className = "carousel-item col-12 col-sm-6 col-md-4 col-lg-3" + (i === 0?' active':'');
+		//myDiv.className = "carousel-item col-xs-6 col-sm-6 col-md-3 col-lg-2" + (i === 0?' active':'');
+		myDiv.className = "carousel-item col-xs-6 col-sm-6 col-md-3 col-lg-2" + (i === 0?' active':'');
 
 		// img
 		let myImg = document.createElement("img");
