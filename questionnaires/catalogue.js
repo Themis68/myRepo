9fdd5578f3694049427f772f2cc31172be306639
parents <parents@mac-mini-de-mini.home>
2020-07-05 16:07:35 +0200

    questionnaires à jour
    Signed-off-by: parents <parents@mac-mini-de-mini.home>

--- questionnaires/catalogue.js
+++ questionnaires/catalogue.js
@@ -1,5 +1,14 @@
 var scenario = [
     {
+        titre: "Révision des bases",
+        poster: "sanction.png",
+        fichier: "bases_03.min.js",
+        variable: "script3",
+        description: "Bases catégories u11 à u15",
+        niveau: 1,
+        temps: 10
+    },
+    {
         titre: "Les joueurs",
         poster: "joueurs.png",
         fichier: "bases_01.min.js",
