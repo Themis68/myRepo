3ab26e0d9c164b4325758f63d1a33d189c06c14c
Themis68 <arretsurimage_@laposte.net>
2021-06-07 18:34:08 +0200

    affichage fanion svg

--- quizz/quizzSP.js
+++ quizz/quizzSP.js
@@ -4,6 +4,9 @@ var pathPosters =  "./images/posters/";		//  posters
 var pathBadges = "./images/badges/";		//  badges
 var pathQuizz = "./questionnaires/";		// scénarios des quizz
 
+var myURLcomplete = document.location.href;
+var myURL  = myURLcomplete.substring( 0 ,myURLcomplete.lastIndexOf( "/" ) );
+
 document.addEventListener("DOMContentLoaded", init, false);	
  
 function init() {
@@ -25,53 +28,61 @@ function creerVignettes(id) {
 	//	myInd.setAttribute("id", "indicateur" + i);
 		myInd.setAttribute("data-target", "#carousel-example");
 		myInd.setAttribute("data-slide-to", i);
-		myInd.setAttribute("class", (i === 0 ? "cercle active" : "cercle"));
+		myInd.setAttribute("class", "cercle"+ (i === 0 ? ' active' : ''));
 		ind.appendChild(myInd);
 	}
 
     
 	// création des vignettes
-	let bloc = document.getElementById(id);
+	let vignettes = document.getElementById(id);
 	
 	for (let i = 0; i < arrayAssoSize(scenario); i++) {
 
-		// div
-		let myDiv = document.createElement("div");
-		myDiv.className = "carousel-item" + (i === 0?' active':'');
+		try {
+		// POSTER
+		let myPoster = document.createElement("img");
+		myPoster.setAttribute("class", "img-fluid mx-auto d-flex");	// nécessaire poour l'affichage des vignettes
+		myPoster.setAttribute("src", pathPosters + scenario[i].poster || "./images/stade.jpg");
 
-		// img
-		let myImg = document.createElement("img");
-		myImg.className = "img-fluid mx-auto d-flex";
-		myImg.setAttribute("alt", "img" + i);
-		myImg.setAttribute("title", "img" + i);
-		myImg.setAttribute("src", pathPosters + (scenario[i].poster || "./images/stade.jpg"));
-/*
+		// assemblage fanion et svg
+		// badge SVG
+		var mySvg = document.createElementNS('http://www.w3.org/2000/svg','svg');
+		mySvg.setAttribute('height','148');
+		mySvg.setAttribute('width','105');
+		mySvg.setAttribute('id','quizz' + scenario[i].id );
+		
+		var svgimg = document.createElementNS('http://www.w3.org/2000/svg','image');
+		svgimg.setAttribute('height','100%');
+		svgimg.setAttribute('width','100%');
+		svgimg.setAttribute('href', pathBadges + "badge" + scenario[i].niveau + ".svg"  || "./images/fff.png");
+
+		mySvg.appendChild(svgimg);
+
 		// caption
 		let myCaption = document.createElement("div");
+		myCaption.setAttribute("class", "carousel-caption");
+		myCaption.appendChild(mySvg);
 
-		// badge
-		let myF = document.createElement("img");
-		myF.className = "carouselFanion";
-		myF.setAttribute("title", "niveau : " + nbQuests[scenario[i].niveau].niv);
-		myF.setAttribute("src", pathBadges + "badge" +(scenario[i].niveau + ".png" || pathImagesCommunes + "fff.png'"));
-		myCaption.appendChild(myF);
-		
-		let myP = document.createElement("p");    
-		myP.innerHTML = scenario[i].titre + " (" + (scenario[i].loi === undefined ? "mix" : "loi " + scenario[i].loi) +")";
-		myCaption.appendChild(myP);
-		myCaption.className = "carousel-caption d-none d-md-block titre";
-		myCaption.setAttribute("onclick", 'javascript:switchQuizz('+ scenario[i].id +');');	// mettre ici car cette DIV est au-dessus de l'image
+		// div
+		let myVignette = document.createElement("div");
+		myVignette.setAttribute("class", "carousel-item" + (i === 0 ? ' active':''));
 
-		myDiv.appendChild(myImg);
-		myDiv.appendChild(myCaption);
-*/
-		myDiv.appendChild(myImg);
-		bloc.appendChild(myDiv);
+		// assembler la vignette
+		myVignette.appendChild(myCaption);
+		myVignette.appendChild(myPoster);
 
+
+		// ajouter à la liste des vignettes
+		vignettes.appendChild(myVignette);
+		/*
 		let myScript = document.createElement("SCRIPT");
 		myScript.setAttribute("type", "text/javascript");
 		myScript.setAttribute("src", pathQuizz + scenario[i].fichier);
 		document.head.appendChild(myScript);
+		*/
+		} catch (err) {
+			alert(err);
+		}
 	}
     
 }
