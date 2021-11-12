6acdc17162bb63cb1003b265833a58ab41da7fa9
Themis68 <arretsurimage_@laposte.net>
2021-11-12 13:18:17 +0100

    integration des images en dynamique

--- quizz/quizzSPslider.js
+++ quizz/quizzSPslider.js
@@ -17,12 +17,16 @@ window.addEventListener('resize', windowResize, false);
 document.addEventListener('readystatechange', ready, false);
 
 function ready() {
-    // interactive
-    // complete
+    // valeurs de l'état : interactive / complete
+	// Cette condition évite le doublement du chargement
 	if (document.readyState === "complete") {
-		// Cette condition évite le doublement du chargement
-		loadSlider();
+		// 1 - on créé les vignettes du slider
 		creerVignettes("holder");
+		// 2 - on met à jour les attributs dynamiques des classes du Slider
+		paramSlider(arrayAssoSize(scenario));
+		// 3 - on lance la fonction sldier
+		loadSlider();
+
 	}
 
 	//showInfosQuizz(scenario[0]);
@@ -37,6 +41,7 @@ function init() {
 function creerVignettes(id) {
 	// création des indicateurs
 	let ind = document.getElementById("indicateurs");
+	console.log(arrayAssoSize(scenario));
 	for (let i = 0; i < arrayAssoSize(scenario); i++) {
 		let myInd = document.createElement("li");
 		myInd.setAttribute("id", "indicateur" + i);
@@ -49,17 +54,21 @@ function creerVignettes(id) {
 	// création des vignettes
 	let holder = document.getElementById(id);
 	for (let i = 0; i < arrayAssoSize(scenario); i++) {
-		// POSTER
-		let myPoster = document.createElement("img");
-		myPoster.setAttribute("class", "slide-image");
+		// mySlideImage
+		let mySlideImage = document.createElement("img");
+		mySlideImage.setAttribute("class", "slide-image");
 		let poster = (scenario[i].loi === undefined ? "bases.png" : "Loi_" + scenario[i].loi + ".png");
-		myPoster.setAttribute("src", pathPosters + poster);
+		mySlideImage.setAttribute("src", pathPosters + poster);
+
+		let mySlide = document.createElement("div");
+		mySlide.setAttribute("class", "slide");
+		mySlide.appendChild(mySlideImage);
 
 		let mySlideWrapper = document.createElement("div");
 		mySlideWrapper.setAttribute("class", "slide-wrapper");
 
 		// ajouter le POSTER
-		mySlideWrapper.appendChild(myPoster);
+		mySlideWrapper.appendChild(mySlide);
 
 		// titre
 		let myTitle = document.createElement("span");
@@ -71,6 +80,26 @@ function creerVignettes(id) {
 
 		// ajouter à la liste des vignettes
 		holder.appendChild(mySlideWrapper);
+	}
+}
+
+function paramSlider(nbSlides) {
+	// .holder
+	let holder = document.getElementsByClassName("holder")[0];
+	holder.style.width = (nbSlides * 100) + "%";
+
+	// .slider-wrap
+	let sliderWrap = document.getElementsByClassName("slider-wrap")[0];
+	sliderWrap.style.marginLeft = ((-nbSlides * 100) /2) + "px";
+	sliderWrap.style.width = (nbSlides * 100) + "px";
+
+	// .slide div
+	let slide = document.getElementsByClassName("slide");
+	// .slide-wrapper
+	let slideWrapper = document.getElementsByClassName("slide-wrapper");
+	for(let i = 0; i < nbSlides; i++) {
+		slide[i].style.width = (nbSlides * 100) + "%";
+		slideWrapper[i].style.width = (100 / nbSlides) + "%";
 	}
 }
 
