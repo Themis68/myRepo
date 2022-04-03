af634a89b7ff237cea3fb5be9f880f38d7b933f1
Themis68 <arretsurimage_@laposte.net>
2022-04-03 18:04:57 +0200

    page disciplines

---
+++ quizz/questionnaires/bases_2451.js
@@ -0,0 +1,134 @@
+var script = [               
+    {
+        number: 1,
+        niveau: 1,
+        loi: 12,
+        question: {
+            libelle: " Que veut dire carton blanc ?",
+            attributs: ["Joueur blessé", "Arrêt de jeu","Sortie d’un joueur durant 10 min.","Penalty"]
+        },
+        reponse: {
+            solution: 3,
+            points: 1
+        }
+    },
+    {
+        number: 2,
+        niveau: 1,
+        loi: 12,
+        question: {
+            libelle: "L’arbitre a-t-il le droit de sanctionner/ exclure un membre du staff ?",
+            attributs: ["Oui","Non","Oui mais seulement l'entraineur","Oui mais seulement en Coupe"]
+        },
+        reponse: {
+            solution: 1,
+            points: 2
+        }
+    },
+    {
+        number: 3,
+        niveau: 1,
+        loi: 13,
+        question: {
+            libelle: "A quelle distance doit être le mur lors d’un coup de pied arrêté ?",
+            attributs: ["8m70","9m15","10m60","7m13"]
+        },
+        reponse: {
+            solution: 2,
+            points: 1
+        }
+    },
+    {
+        number: 4,
+        niveau: 1,
+        loi: 11,
+        question: {
+            libelle: "Il n’y a pas d’Hors-jeu lors lors ...",
+            attributs: ["un coup d'envoi","une touche","un coup-franc","un corner"]
+        },
+        reponse: {
+            solution: 2,
+            points: 2
+        }
+    },
+    {
+        number: 5,
+        niveau: 1,
+        loi: 13,
+        question: {
+            libelle: "Lors d’un coup franc joué rapidement, l'adversaire peut ...",
+            attributs: ["Géner l'action, ne sera pas sanctionné","Géner l'action, mais prend le risque d'être sanctionné","Géner l'action et sera averti verbalement","Toucher le ballon mis à terre"]
+        },
+        reponse: {
+            solution: 2,
+            points: 2,
+            termps: 15
+        }
+    },
+    {
+        number: 6,
+        niveau: 1,
+        loi: 13,
+        question: {
+            libelle: "Un joueur n'est pas à distance réglementaire lors d'un coup-franc. Que peut siffler l'arbitre ?",
+            attributs: ["L’expulsion du joueur qui n’a pas respecté la distance","Un carton jaune pour le joueur","Un coup franc indirect","Un penalty"]
+       },
+        reponse: {
+            solution: 3,
+            points: 3,
+            temps: 10
+        }
+    },
+    {
+        number: 7,
+        niveau: 1,
+        loi: 12,
+        question: {
+            libelle: "Par quelle sanction, l'arbitre indique une exclusion définitive d'un joueur ?",
+            attributs: ["carton rouge","carton blanc","carton jaune","carton noir"]
+        },
+        reponse: {
+            solution: 1,
+            points: 1
+        }
+    },
+    {
+        number: 8,
+        niveau: 1,
+        loi: 11,
+        question: {
+            libelle: "En cas de hors-jeu, qu'accorde l'arbitre ? ",
+            attributs: ["Une touche","Un coup-franc direct","Un coup franc indirecte à l'endroit où la faut a été comise","Un 6m"]
+        },
+        reponse: {
+            solution: 3,
+            points: 3
+        }
+    },
+    {
+        number: 9,
+        niveau: 1,
+        loi: 14,
+        question: {
+            libelle: "Qu'est-ce qu'un coup de pied de réparation ? ",
+            attributs: ["Un coup franc direct","Un 6m","Un penalty","Un coup-franc indirect"]
+        },
+        reponse: {
+            solution: 2,
+            points: 2
+        }
+    },
+    {
+        number: 10,
+        niveau: 1,
+        loi: 11,
+        question: {
+            libelle: "Laquelle de ces fautes est passible d'une exclusion ?",
+            attributs: ["Pénètre et quitte le terain sans autorisation préalable","Retarde la reprise du jeu","Provoque une faute dans une action de but","Manifeste sa désapprobation en paroles ou en actes"]
+        },
+        reponse: {
+            solution: 3,
+            points: 4
+        }
+    }
+];
