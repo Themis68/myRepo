f5400fb712514743255523d07c76c3adfd82b43b
Themis68 <arretsurimage_@laposte.net>
2022-06-23 19:26:08 +0200

    new quizz foot 11

--- quizz/questionnaires/catalogue.js
+++ quizz/questionnaires/catalogue.js
@@ -19,77 +19,47 @@ var scenario = [
     },
     {
         id: 3,
+        titre: "Football à 11",
+        fichier: "foot11_5380.js",
+        variable: "script",
+        description: "Les bases du football à 11",
+        niveau: 1,
+        temps: 15
+    },
+    {
+        id: 4,
         titre: "Les bases sur les reprises de jeu",
         fichier: "bases_2451.js",
         variable: "script",
         description: "Touches, Corners, Coup de pied de but",
         niveau: 1,
         temps: 15
-    },
-    {
-        id: 4,
-        titre: "Les bases sur les éléments du matchs",
-        fichier: "bases_2274.js",
-        variable: "script",
-        description: "Mix sur les éléments liés au match",
-        niveau: 1,
-        temps: 12
     },
     {
         id: 5,
-        titre: "Les bases sur les acteurs du football",
-        fichier: "bases_5547.js",
-        variable: "script",
-        description: "Joueurs et arbitres",
-        niveau: 1,
-        temps: 8
-    },
-    {
-        id: 6,
-        titre: "Les bases des lois 7 à 10",
-        fichier: "bases_6384.js",
-        variable: "script",
-        description: "Bases sur les lois Duréee, Coup d'envoi, Ballon et issue du match",
-        niveau: 1,
-        temps: 15
-    },
-]
-/*var scenario = [
-    {
-        id: 1,
-        titre: "Les bases sur les acteurs du football",
-        fichier: "bases_5547.js",
-        variable: "script",
-        description: "Joueurs et arbitres",
-        niveau: 1,
-        temps: 8
-    },
-    {
-        id: 2,
-        titre: "Les bases des lois 7 à 10",
-        fichier: "bases_6384.js",
-        variable: "script",
-        description: "Bases sur les lois Duréee, Coup d'envoi, Ballon et issue du match",
-        niveau: 1,
-        temps: 15
-    },
-    {
-        id: 3,
-        titre: "Les bases sur les reprises de jeu",
-        fichier: "bases_2451.js",
-        variable: "script",
-        description: "Touches, Corners, Coup de pied de but",
-        niveau: 1,
-        temps: 10
-    },
-    {
-        id: 4,
         titre: "Les bases sur les éléments du matchs",
         fichier: "bases_2274.js",
         variable: "script",
         description: "Mix sur les éléments liés au match",
         niveau: 1,
         temps: 12
-    }
+    },
+    {
+        id: 6,
+        titre: "Les bases sur les acteurs du football",
+        fichier: "bases_5547.js",
+        variable: "script",
+        description: "Joueurs et arbitres",
+        niveau: 1,
+        temps: 8
+    },
+    {
+        id: 7,
+        titre: "Les bases des lois 7 à 10",
+        fichier: "bases_6384.js",
+        variable: "script",
+        description: "Bases sur les lois Duréee, Coup d'envoi, Ballon et issue du match",
+        niveau: 1,
+        temps: 15
+    },
 ]
-*/
