90c5b43a7492f0ec9fa04daa6b4028a2dab83247
Themis68 <arretsurimage_@laposte.net>
2023-04-16 11:27:10 +0200

    quizz foot 5

---
+++ desk/quizz/scenarios/bases_04.js
@@ -0,0 +1,135 @@
+var script3 = [               
+    {
+        number: 1,
+        niveau: 1,
+        question: {
+            libelle: "Quelle taille de ballon est autorisée ?",
+            attributs: ["Taille 3","Taille 4","Taille 5","Taille libre"]
+        },
+        reponse: {
+            solution: 2,
+            loi: 2,
+            points: 1
+        }
+    },
+    {
+        number: 2,
+        niveau: 1,
+        question: {
+            libelle: "Quelle est la durée d'un match ?",
+            attributs: ["2 x 15 min","2 x 20 min","2 x 25 min","2 x 30 min"]
+        },
+        reponse: {
+            solution: 3,
+            loi: 7,
+            points: 1
+        }
+    },
+    {
+        number: 3,
+        niveau: 1,
+        question: {
+            libelle: "Quel est le nombre de remplaçants maximum ?",
+            attributs: ["0","2","4","5"]
+        },
+        reponse: {
+            solution: 3,
+            loi: 3,
+            points: 1
+        }
+    },
+    {
+        number: 4,
+        niveau: 1,
+        question: {
+            libelle: "Quelle catégorie de joueurs peut arbitrer en priorité ces matchs ?",
+            attributs: ["u13-u14","u15-u17","u18","Séniors"]
+        },
+        reponse: {
+            solution: 2,
+            loi: 5,
+            points: 4
+        }
+    },
+    {
+        number: 5,
+        niveau: 1,
+        question: {
+            libelle: "Où se situe la zone de hors-jeu ?",
+            attributs: ["Surface de réparation","Milieu de terrain","Zone de but","Il n'y en a pas"]
+        },
+        reponse: {
+            solution: 1,
+            loi: 11,
+            points: 2
+        }
+    },
+    {
+        number: 6,
+        niveau: 1,
+        question: {
+            libelle: "Comment l'arbitre indique-t-il un coup-franc indirect ?",
+            attributs: ["Il tend le bras vers l'endroit de la faute","Il tend le bras vers le camp de l'équipe sanctionnée","Il lève le bras puis le baisse","Il maintient le bras levé jusqu'à l'exécution du coup-franc"]
+        },
+        reponse: {
+            temps: 15,
+            solution: 4,
+            loi: 12,
+            points: 4
+        }
+    },
+    {
+        number: 7,
+        niveau: 1,
+        question: {
+            libelle: "A quelle distance doivent se trouver les adversaires lors d'un coup-franc indirect ?",
+            attributs: ["4 m","5 m","6 m","9 m 15"]
+        },
+        reponse: {
+            solution: 3,
+            loi: 13,
+            points: 2
+        }
+    },
+    {
+        number: 8,
+        niveau: 1,
+        question: {
+            libelle: "Où doit-on positionner le ballon lors d'un coup de pied de but ?",
+            attributs: ["A 1 m à gauche ou à droite du point de Penalty","Sur le point de Penalty","A la limite de la surface de réparation","Position libre dans la surface de réparation"]
+        },
+        reponse: {
+            temps: 15,
+            solution: 1,
+            loi: 16,
+            points: 4
+        }
+    },
+    {
+        number: 9,
+        niveau: 1,
+        question: {
+            libelle: "A quelle distance du but se trouve le point de Penalty ?",
+            attributs: ["6 m","9 m","9 m 15","11 m"]
+        },
+        reponse: {
+            solution: 2,
+            loi: 14,
+            points: 2
+        }
+    },
+    {
+        number: 10,
+        niveau: 1,
+        question: {
+            libelle: "Dans quel cas un arbitre peut-il revenir sur une décision ?",
+            attributs: ["Jamais","Seulement s'il s'agit d'un but accordé par erreur","Seulement si le jeu n'a pas repris","seulement sur suggestion de l'arbitre assistant"]
+        },
+        reponse: {
+            temps: 15,
+            solution: 3,
+            loi: 5,
+            points: 5
+        }
+    },
+]
--- desk/quizz/scenarios/catalogue.js
+++ desk/quizz/scenarios/catalogue.js
@@ -1,6 +1,5 @@
 var scenario = [
         {
-            
             id: 1,
             titre: "Les bases du foot à 8 u10-u11",
             poster: "bases2.jpg",
@@ -13,35 +12,11 @@ var scenario = [
         },
         {
             id: 2,
-            titre: "Les joueurs",
-            poster: "joueurs.png",
-            fichier: "bases_01.js",
+            titre: "Les bases du foot à 5 u8-u9",
+            poster: "bases2.jpg",
+            fichier: "bases_04.js",
             variable: "script1",
-            description: "Questionnaire sur les joueurs",
-            loi: 3,
-            niveau: 1,
-            tempsQuestion: 10,
-            tempsReponse: 10
-        },
-        {
-            id: 3,
-            titre: "Les joueurs",
-            poster: "sanction.png",
-            fichier: "bases_01.js",
-            variable: "script1",
-            description: "Questionnaire sur les joueurs",
-            loi: 3,
-            niveau: 2,
-            tempsQuestion: 10,
-            tempsReponse: 10
-        },
-        {
-            id: 4,
-            titre: "Les bases",
-            poster: "corner.png",
-            fichier: "bases_02.js",
-            variable: "script2",
-            description: "Questionnaire sur les bases de l'arbitrage",
+            description: "Questionnaire pour le tournoi Georges Masson",
             niveau: 1,
             tempsQuestion: 10,
             tempsReponse: 10
