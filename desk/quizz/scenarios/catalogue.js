047bccdcf2e91c28fb6852a1fb43ad627e8a7c3c
Themis68 <arretsurimage_@laposte.net>
2023-04-16 14:19:48 +0200

    correction indicateurs

--- desk/quizz/scenarios/catalogue.js
+++ desk/quizz/scenarios/catalogue.js
@@ -20,5 +20,27 @@ var scenario = [
             niveau: 1,
             tempsQuestion: 10,
             tempsReponse: 10
+        },
+        {
+            id: 3,
+            titre: "inactif",
+            poster: "gardien.png",
+            fichier: "bases_03.js",
+            variable: "script3",
+            description: "Questionnaire pour le tournoi Georges Masson",
+            niveau: 1,
+            tempsQuestion: 10,
+            tempsReponse: 10
+        },
+        {
+            id: 4,
+            titre: "inactif",
+            poster: "corner.png",
+            fichier: "bases_04.js",
+            variable: "script4",
+            description: "Questionnaire pour le tournoi Georges Masson",
+            niveau: 1,
+            tempsQuestion: 10,
+            tempsReponse: 10
         }
 ]
