ed9a963ec3fb91b5d275f945abc9c1af66697887
Themis68 <arretsurimage_@laposte.net>
2023-05-09 22:51:01 +0200

    Réintégration des questionnaires des mobiles

---
+++ mob/quizz/questionnaires/bases_5547.js
@@ -0,0 +1,152 @@
+var script = [               
+    {
+        number: 1,
+        niveau: 1,
+        loi: 3,
+        question: {
+            libelle: "En foot à 11, combien de joueurs peuvent être sur la feuille de match ?",
+            attributs: ["13","14","15","18"],
+            temps: 5
+        },
+        reponse: {
+            solution: 2,
+            points: 1,
+            temps: 10
+        }
+    },
+    {
+        number: 2,
+        niveau: 1,
+        loi: 3,
+        question: {
+            libelle: "Pour quelle situation un remplacement est-il possible ?",
+            attributs: ["n'importe quand","lorsque le gardien intercepte le ballon","lors d'un coup-franc","lors d'un arrêt de jeu"],
+            temps: 5
+        },
+        reponse: {
+            solution: 4,
+            points: 1,
+            temps: 10
+        }
+    },
+    {
+        number: 3,
+        niveau: 1,
+        loi: 3,
+        question: {
+            libelle: "En football amateur, combien de fois un remplaçant peut il rentrer en jeu ?",
+            attributs: ["Autant de fois que l'on veut","1 fois par match","1 fois par mi-temps","3 fois maximum"],
+            temps: 5
+        },
+        reponse: {
+            solution: 1,
+            points: 1,
+            temps: 10
+        }
+    },
+    {
+        number: 4,
+        niveau: 1,
+        loi: 4,
+        question: {
+            libelle: "Quel objet un joueur peut il porter pendant un match : bracelet, chaîne, élastique pour les cheveux, serre-tête ?",
+            attributs: ["Une chaine","Un bracelet","Un serre-tête","Un élastique pour les cheveux"],
+            temps: 5
+        },
+        reponse: {
+            solution: 4,
+            points: 1,
+            temps: 15
+        }
+    },
+    {
+        number: 5,
+        niveau: 1,
+        loi: 4,
+        question: {
+            libelle: "S'il fait froid, un joueur peut il porter un sous-maillot à manche longue sous un maillot à manche courte ?",
+            attributs: ["Non","Oui quelle que soit la couleur du sous-maillot","Oui si le sous-maillot est de la même couleur du maillot","Oui si le sous-maillot est de couleur blanche"],
+            temps: 5
+        },
+        reponse: {
+            solution: 3,
+            points: 3,
+            temps: 15
+        }
+    },
+    {
+        number: 6,
+        niveau: 1,
+        loi: 5,
+        question: {
+            libelle: "Au niveau district, quelles couleurs de carton n'existent pas ?",
+            attributs: ["Jaune","Noir","Blanc","Rouge"],
+            temps: 5
+        },
+        reponse: {
+            solution: 2,
+            points: 1,
+            temps: 10
+        }
+    },
+    {
+        number: 7,
+        niveau: 1,
+        loi: 5,
+        question: {
+            libelle: "Dans quel cas, un arbitre doit il laisser le jeu se poursuivre ?",
+            attributs: ["S'il n'y pas plus assez de visibilité sur le terrain","Si une personne supplémentaire est rentrée sur le terrain","Si un joueur lui parle pendant le match","Si un second ballon entre sur le terrain"],
+            temps: 5
+        },
+        reponse: {
+            solution: 3,
+            points: 2,
+            temps: 20
+        }
+    },
+    {
+        number: 8,
+        niveau: 1,
+        loi: 6,
+        question: {
+            libelle: "Quelle est la position du drapeau de l'arbitre assistant pour une touche ?",
+            attributs: ["drapeau vers le haut","drapeau vers le bas","drapeau parallèle à la ligne de touche en indiquant le sens du but de l'équipe adverse ","drapeau parallèle à la ligne de touche en indiquant le sens du but de l'équipe possédant le ballon"],
+            temps: 5
+        },
+        reponse: {
+            solution: 3,
+            points: 1,
+            temps: 15
+        }
+    },
+    {
+        number: 9,
+        niveau: 1,
+        loi: 6,
+        question: {
+            libelle: "Que ne peut pas faire un arbitre assistant ?",
+            attributs: ["Donner un carton à un joueur","Signaler un hors-jeu","Signaler une touche","Signaler un corner"],
+            temps: 5
+        },
+        reponse: {
+            solution: 1,
+            points: 1,
+            temps: 10
+        }
+    },
+    {
+        number: 10,
+        niveau: 1,
+        loi: 4,
+        question: {
+            libelle: "En matière de protection, les protèges-tibias",
+            attributs: ["sont obligatoires et peuvent êtres partiellement recouverts","ne sont pas obligatoires","sont obligatoires et peuvent êtres découverts","sont obligatoires et doivent êtres recouverts totalement"],
+            temps: 5
+        },
+        reponse: {
+            solution: 4,
+            points: 1,
+            temps: 15
+        }
+    }
+];
