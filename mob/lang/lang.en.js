369bcc500f7dc3c21ca66bee7622d2ad63da3f7f
Themis68 <arretsurimage_@laposte.net>
2023-05-12 17:07:47 +0200

    ajout langue anglaise

---
+++ mob/lang/lang.en.js
@@ -0,0 +1,13 @@
+lang = {
+    LIB_A001:"Discover new emotions by becoming a referee of your favorite sport",
+    LIB_A002:"Football",
+    LIB_A003:"Futsal",
+    LIB_A004:"Basketball",
+    LIB_A005:"Handball",
+    LIB_B001:"Choose a quiz to try your hand at becoming a referee",
+    LIB_B002:"Launch the Quiz",
+    LIB_C001:"Your browser does not support sound files",
+    LIB_C002:"Next question",
+    LIB_C003:"See result",
+    LIB_D001:"Mail result"
+}
