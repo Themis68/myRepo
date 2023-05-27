4b38ce8701d9a362f00091b9f50d44e5961c8a80
Themis68 <arretsurimage_@laposte.net>
2023-05-27 19:49:49 +0200

    modularisation des langues

--- mob/indexSP.js
+++ mob/indexSP.js
@@ -10,28 +10,45 @@ var pathLangues = "./lang";
 document.addEventListener("DOMContentLoaded", init, false);	
 window.addEventListener('resize', windowResize, false); 
 document.addEventListener('readystatechange', ready, false);
+window.addEventListener('load', windowLoaded, false); 
+/*
+window.addEventListener('load', (event) => {
+    console.log("event 3");
+    // LANG : appeler l'initialisation
+    LG_load();      // on créé le code HTML pour le menu des langues
+    //LG_selectLangue("../mob/lang", paramsURL.lang);	// on charge les chaines dns la langue souhaitée
+});
+*/
+function windowLoaded() {
+    console.log("event 3");
+    // LANG : appeler l'initialisation
+}
+
 
 function ready() {
+    console.log("event 1");
+    // event 1
+
     // Cette condition évite le doublement du chargement
-	if (document.readyState === "complete") {
+/*	if (document.readyState === "complete") {
         setLibelle("titre","LIB_A001");
         setLibelle("titre1","LIB_A002");
         setLibelle("titre2","LIB_A003");
         setLibelle("titre3","LIB_A004");
         setLibelle("titre4","LIB_A005");
         setLibelle("asi-title","LIB_A006");
-    }
+    }*/
 }
 
 /**
  * s'exécute au lancement de la page
  */
 function init() {
+    console.log("event 2");
+    // passe après READY()
     // calcul hauteur
     matriceDevice = viewportSize();
     calculHauteur(matriceDevice.height - 200);
     setVersion(version);
     paramsURL = getParameters();	// on récupère un tableau associatif depuis les paramètres de l'URL
-    genererHtml();      // on créé le code HTML pour le menu des langues
-	selectLangue("../mob/lang", paramsURL.lang);	// on charge les chaines dns la langue souhaitée
 }
--- mob/lang/lang.js
+++ mob/lang/lang.js
@@ -1,30 +1,122 @@
-var version = "1.0.0";
+var version = "1.0.1";
 
+// variables
+var LG_params = [
+	{id: "fr", flag:"FR"},
+	{id: "pt", flag:"PT"},
+	{id: "en", flag:"EN"}
+];
+
+var LG_defaut = {id: "fr", flag:"FR"};
+
+// déclaration des objets
+var LG_codeHtml = "";
+var LG_menu_lang_title = "";
+var LG_icone_lang = "";
+
+// URI complète
 var myURLcomplete = document.location.href;
+// URL sans les paramètres
 var myURL  = myURLcomplete.substring( 0 ,myURLcomplete.lastIndexOf( "?" ) );
 
 // gestion du clic
 document.addEventListener("touchstart", clickF, false);
+window.addEventListener('load', LG_load, false); 
 
+document.addEventListener("DOMContentLoaded", init, false);	 
 document.addEventListener('readystatechange', ready, false);
 
-function ready() {
-	if (document.readyState === "complete") {
-		// afficher la langue
-		paramsURL = getParameters();
-		let obj = document.getElementById("menu-lang-title");
-		let maj = (paramsURL.lang).toUpperCase();
-		obj.innerHTML = " " + maj;
-		// afficher icone
-		let objImg = document.getElementById("icone-lang");
-		let chemin = pathLangues + "/images/" + maj + ".png";
-		console.log(chemin);
-		objImg.setAttribute("src", chemin);
+function init(){
+	console.log("init");
+}
 
+function ready(){
+	console.log("ready");
+}
+
+function LG_load() {
+	// on insere le code sur la page HTML
+	// objets
+	LG_codeHtml = document.getElementById("menu-lang");					// flag qui confirme l'usage de langue apr la page HTML
+
+	if (LG_codeHtml != null) {
+		LG_genererHtml(window.LG_params);
+		// MAJ de la langue par defaut
+		LG_displayLang(window.LG_defaut);
 	}
 }
 
-function selectLangue(pathLangues, langue){
+// retourne la taille d'un tableau associatif
+//
+// exemple appel : arrayAssoSize(scenario) avec 
+// - scenario un tableau associatif
+//
+function LG_arrayAssoSize(arr) {
+    var size = 0;
+    for (var key in arr) 
+    {
+        if (arr.hasOwnProperty(key)) size++;
+    }
+    return size;
+}
+
+// retourne l'indice du tableau correspondnat à la valeur reçue
+//
+// exemple appel : arrayAssoSearch(actions, timeCode) avec 
+// - actions un tableau associatif
+// - timecode la valeur à rechercher
+//
+function LG_arraySearch(arr, valObject) {
+	var nbEl = LG_arrayAssoSize(arr);
+	for (var ind = 0; ind < nbEl; ind++) {
+		if (arr[ind].id === valObject) {
+			return ind;	// retourne l'indice du tableau
+		}
+	}
+	return -1;	// aucun résultat
+}
+
+function LG_displayLang(langue) {
+	window.LG_menu_lang_title = document.getElementById("menu-lang-title");	// titre de la langue à afficher
+	window.LG_icone_lang = document.getElementById("icone-lang");				// drapeau de la langue
+
+	// afficher langue
+	window.LG_menu_lang_title.innerHTML = "&nbsp;" + langue.flag;
+	// afficher icone
+
+	// comment gérer le chemin d'accès de la page HTML ?
+	// on peut le récupérer ?
+	// on le passe en paralètre ?
+	let chemin = pathLangues + "/images/" +langue.id + ".png";
+	window.LG_icone_lang.setAttribute("src", chemin);
+}
+
+function LG_ready(codeLang) {
+
+	// intégration du fichier de langue sélectionné
+	LG_selectLangue("../mob/lang", paramsURL.lang);	// on charge les chaines dns la langue souhaitée
+
+	// récupérer les paramètres dans l'URL
+	let params = getParameters();
+
+	// on vérifie si la langue est gérée
+	let indice = LG_arraySearch(window.LG_params, paramsURL.lang);
+	if( indice > -1) {
+		// on affiche le nom court de la langue
+		let span = document.createElement("span");
+		span.innerHTML = " " + (LG_params[indice].flag);
+		window.LG_menu_lang_title.appendChild(span);
+
+		// afficher icone
+		let chemin = pathLangues + "/images/" + params.lang + ".png";
+		window.LG_icone_lang.setAttribute("src", chemin);
+	} else {
+		// afficher une erreur
+		window.alert("La langue "+params.lang + " n'est pas prise en charge");
+	}
+}
+
+function LG_selectLangue(pathLangues, langue){
 	// ajout accès au fichier des questions	
 	let myScript = document.createElement("script");
 	myScript.type = "text/javascript";
@@ -58,22 +150,68 @@ function clickF (e) {
 	}
 }
 
-function genererHtml() {
-	let html='<label for="menu-cb" class="menu-label">';
-   		html+='<img class="menu-item-img" id="icone-lang" src=""/>';
-    	html+='<span id="menu-lang-title"></span>';
-    html+='</label>';
-    html+='<input type="checkbox" id="menu-cb" class="menu-cb">';
-    html+='<nav class="menu-nav">';
-    	html+='<ul>';
-    		html+='<li class="menu-item"><a id="sel-lang-fr" href=""><img id="icone-lang-fr" class="menu-item-img" src="./lang/images/FR.png" /> FR</a></li>';
-    		html+='<li class="menu-item"><a id="sel-lang-pt" href=""><img id="icone-lang-pt" class="menu-item-img" src="./lang/images/PT.png" /> PT</a></li>';
-    		html+='<li class="menu-item"><a id="sel-lang-en" href=""><img id="icone-lang-en" class="menu-item-img" src="./lang/images/EN.png" /> EN</a></li>';
-    	html+='</ul>';
-    html+='</nav>';
+function LG_genererHtml(params) {
+	// bin$ome : drapeau et abbréviation
+	let label  = document.createElement("label");
+    label.setAttribute("for", "menu-cb");
+	label.setAttribute("class", "menu-label");
 
-	let obj = document.getElementById("menu-lang");
-	obj.innerHTML = html;
+	// drapeau
+	let img  = document.createElement("img");
+	img.setAttribute("class", "menu-item-img");
+	img.setAttribute("id", "icone-lang");
+	img.setAttribute("src", "");
+
+	// langue abrégée
+	let span  = document.createElement("span");
+	span.setAttribute("id", "menu-lang-title");
+	span.setAttribute("class", "menu-item-span");
+	label.appendChild(img);
+	label.appendChild(span);
+
+	window.LG_codeHtml.appendChild(label);
+
+	// case a cocher
+	let input  = document.createElement("input");
+    input.setAttribute("type", "checkbox");
+	input.setAttribute("id", "menu-cb");
+	input.setAttribute("class", "menu-cb");
+
+	window.LG_codeHtml.appendChild(input);
+	// menu déroulant
+	let nav = document.createElement("nav");
+    nav.setAttribute("class", "menu-nav");
+
+	// boucle pour les langues
+	let ul = document.createElement("ul");
 	
+	for (i = 0; i < LG_arrayAssoSize(params); i++){
+		// langue
+		let li  = document.createElement("li");
+		li.setAttribute("class", "menu-item");
+
+		// kien hypertext
+		let hyp = document.createElement("a");
+		hyp.setAttribute("id", "sel-lang-" + params[i].id);
+		hyp.setAttribute("class", "menu-label");
+		hyp.setAttribute("href", "");
+		// img
+		let img1 = document.createElement("img");
+		img1.setAttribute("id", "icone-lang-" + params[i].id);
+		img1.setAttribute("class", "menu-item-img");
+		img1.setAttribute("src", "./lang/images/"+ params[i].flag + ".png");
+
+		let span = document.createElement("span");
+		span.setAttribute("class", "menu-item-span");
+		span.innerHTML = "&nbsp;"+ params[i].flag;
+
+		hyp.appendChild(img1);
+		hyp.appendChild(span);
+		li.appendChild(hyp);
+		ul.appendChild(li);
+
+	}
+	nav.appendChild(ul);
+	window.LG_codeHtml.appendChild(nav);
 }
 
