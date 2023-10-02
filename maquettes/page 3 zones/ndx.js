3dab1d3f89b07c11e0bc83895ff3ea647006cc86
Themis68 <thx2013net@laposte.net@laposte.net>
2023-10-02 08:51:43 +0200

    MAJ maquette 3 zones

--- maquettes/page 3 zones/ndx.js
+++ maquettes/page 3 zones/ndx.js
@@ -1,27 +1,67 @@
+// chainage avec les template
+ // var template = import("template.js");
+ // import {temple} from "template.js";
+ //const Template = require('template.js');
+
 // écouteurs
 document.addEventListener('readystatechange', ready, false);
-document.addEventListener("DOMContentLoaded", init, false);	
-window.addEventListener('load', windowLoaded, false); 
 
-function init(){
-    // recupérer infos taille viewport
-    let matriceDevice = getViewportSize();
-    console.log("3 - INIT : ", matriceDevice);
-}
+// on choisi d'afficher ou amsquer le menu des langues
+// on aura dinc la langue par défaut
+const afficherMenuLangue = true;
+LG_.setMenu(afficherMenuLangue);
+
 
 function ready() {
     // recupérer infos taille viewport
     let matriceDevice = getViewportSize();
-    if (document.readyState === "complete") {
-        console.log("5 - READY 100% ", matriceDevice)
-    } else {
-        console.log("1 - READY ", matriceDevice);
-        // récupérer template Header
-        let header = document.getElementById("tmp_header");
-        header.innerHTML = tmp_header("LAB_A006");  // on passe la clé du titre tmp_header("LAB_A006")
+    switch(document.readyState) {
+        case "uninitialized":   // Has not started loading
+            // apparait en cas de ralentissement
+            console.log("1 HTML");   // ne passe aps à cet étape
+            break;
+        case "loading":         // Is loading
+            // apparait en cas de ralentissement
+            console.log("2 HTML");   // ne passe aps à cet étape
+            /*
+            L'évènement DOMContentLoaded est déclenché quand le document HTML initial est complètement chargé et analysé, 
+            sans attendre la fin du chargement des feuilles de styles, images et sous-document.
+            */
+            document.addEventListener("DOMContentLoaded", DOMContentLoaded, false);	
+            break;
+        case "loaded":          // Has been loaded
+            console.log("3 HTML");   // ne passe pas à cet étape
+            break;
+        case "interactive":     // Has loaded enough to interact with
+            console.log("4 HTML");
+            // on voit déjà les objets en dur dans la page
+            // HEADER
+            const myTemplate = new Template();
+            const myHeader =  document.getElementById('tmp_header');
+            // afficher/masquer le menu des Langues ?
+            myHeader.innerHTML = myTemplate.header("LAB_A006", afficherMenuLangue);
+            break;
+        case "complete":        // Fully loaded
+            console.log("5 HTML");
+
+            break;
+        default:
+
     }
 }
 
-function windowLoaded() {
-    console.log("7 - LOAD");
+
+
+function DOMContentLoaded(){
+    // ne passe pas à cette étape
+    console.log("DOMContentLoaded");
 }
+
+/*
+window.addEventListener('load', (event) => {
+    let header = document.getElementsByTagName("footer")[0];
+    let headerHeight = header.getBoundingClientRect().height;
+    //let useHeight = window.innerHeight - headerHeight - footerHeight;
+    header.style.maxHeight = headerHeight + "px";
+});
+*/
--- ndx.js
+++ ndx.js
@@ -3,10 +3,14 @@
  // import {temple} from "template.js";
  //const Template = require('template.js');
 
- console.log("esd");
-
 // écouteurs
 document.addEventListener('readystatechange', ready, false);
+
+// on choisi d'afficher ou amsquer le menu des langues
+// on aura dinc la langue par défaut
+const afficherMenuLangue = true;
+LG_.setMenu(afficherMenuLangue);
+
 
 function ready() {
     // recupérer infos taille viewport
@@ -34,9 +38,8 @@ function ready() {
             // HEADER
             const myTemplate = new Template();
             const myHeader =  document.getElementById('tmp_header');
-            // on ne veut pas le menu des Langues
-            const afficherMenuLang = false; 
-            myHeader.innerHTML = myTemplate.header("LAB_A006", afficherMenuLang);
+            // afficher/masquer le menu des Langues ?
+            myHeader.innerHTML = myTemplate.header("LAB_A006", afficherMenuLangue);
             break;
         case "complete":        // Fully loaded
             console.log("5 HTML");
