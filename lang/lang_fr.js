00c74780ea80b1b690148b8e1105d28ac3cd3170
Themis68 <thx2013net@laposte.net@laposte.net>
2024-01-07 15:01:22 +0100

    affichage des indicateurs

--- lang/lang_fr.js
+++ lang/lang_fr.js
@@ -1,11 +1,11 @@
 LG_lang = {
     LAB_A006:"Disciplines", 
-    LAB_A001:"Découvre de nouvelles émotions en devenant arbitre de ton sport préféré",
+    LAB_A001:"Choisis une discipline ",
     LAB_A002:"Football",
     LAB_A003:"Futsal",
     LAB_A004:"Basketball",
     LAB_A005:"Handball",
-    LAB_B001:"Choisis un quizz pour t'essayer à devenir arbitre",
+    LAB_B001:"Choisis un questionnaire",
     LAB_B002:"Lancer le Quizz",
     LAB_B003:"Catalogue",
     LAB_B004:"Description",
--- lang/lang_pt.js
+++ lang/lang_pt.js
@@ -1,11 +1,11 @@
 LG_lang = {
     LAB_A006:"Desportos",
-    LAB_A001:"Descobre novas emoções tornando-se um árbitro do teu esporte favorito",
+    LAB_A001:"Escolhe um desporto",
     LAB_A002:"Futebol",
     LAB_A003:"Futsal",
     LAB_A004:"Basquetebol",
     LAB_A005:"Handebol",
-    LAB_B001:"Escolhe um Teste para tentar se tornar um árbitro",
+    LAB_B001:"Escolhe um Teste",
     LAB_B002:"Inicia o Teste",
     LAB_B003:"Disciplina",
     LAB_B004:"Descriçao",
