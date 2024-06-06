76dfb9a34b300c8a51db7d41c193f159200f80af
  themis68 <arretsurimage_@laposte.net>
2024-06-06 22:53:56 +0200

    intégration de la vidéo analyse

---
+++ desk/rencontres/scenarios/EMouz_Herbiers.js
@@ -0,0 +1,66 @@
+var script2 = [             
+    {
+        step: '00:00:03',
+        act: "AllerA",
+        niveau: "CONFIRME",
+        indice: "00:00:28"
+    },
+    {
+        step: '00:00:05',
+        act: "Question",
+        niveau: "DEBUTANT",
+        question: {
+            libelle: "Selon vous y-a-t-il hors-jeu des blanches ? "
+        },
+        attributs: ["Oui", "Non"],
+        reponse: {
+            solution: 1,
+            libelle: "L'arbitre assistant signale que la joueuse de l'équipe Blanche qui reçoit le ballon est en position de hors-jeu",
+            points: 1,
+            pict: "arbitre/hors-jeu.png",
+            loi: "Loi_11"
+        }
+    },
+    {
+        step: '00:00:15',
+        act: "Bonus",
+        niveau: "DEBUTANT",
+        saut: {
+            attente: 3,
+            indice: '00:05:32'
+        },
+        question: {
+            libelle: "Quel est le carton que sort l'arbitre lorsqu'il expulse un joueur? "
+        },
+        attributs: ["Jaune", "Rouge", "Blanc", "Vert"],
+        reponse: {
+            solution: 2,
+            points: 1,
+            loi: "Loi_11"
+        }
+    },
+    {
+        step: '00:00:36',
+        act: "Question",
+        niveau: "CONFIRME",
+        saut: {
+            attente: 5,
+            indice: '00:05:32'
+        },
+        question: {
+            libelle: "Que va proposer l'arbitre sur cette faute sifflée ? "
+        },
+        attributs: ["Coup-Franc direct", "Coup-Franc indirect","Balle à terre"],
+        reponse: {
+            solution: 1,
+            libelle: "L'arbitre lève le bras pour signaler la faute. Puis le baisse pour indiquer le caractère direct du coup-franc",
+            pict: "direct.png",
+            points: 2,
+            loi: "Loi_11"
+        }
+    },
+    {
+        step: '00:05:36',
+        act: "Fin"
+    }
+]
---
+++ desk/rencontres/scenarios/catalogue.js
@@ -0,0 +1,106 @@
+var scenario = [
+    [
+        {
+            id: 1,
+            rencontre: "Bretagne/PDLL",
+            poster: "Pole_PloufraganMT1.png",
+            fichier: "Pole_PloufraganMT1.mp4",
+            scenario: "Pole_PloufraganMT1.js",
+            variable: "script1",
+            description: "Match amical opposant Les Pôles espoirs des Ligues de Bretagne (en blanc) et des Pays de La Loire (en bleu marine) - 1ère période",
+            gauche: {
+                nom: "Ligue de Bretagne",
+                fanion: "ligueBretagne.png",
+                site: "https://footbretagne.fff.fr",
+                maillotCouleur: "rgb(255, 255, 255)"
+            },
+            droite: {
+                nom: "Ligue Pays De la Loire",
+                fanion: "Ligue2.png",
+                site: "https://lfpl.fff.fr",
+                maillotCouleur: "rgb(67, 76, 108)"
+            },
+            arbitre: {
+                maillotCouleur: "rgb(255, 112, 119)"
+            }
+        }
+    ],
+    [
+        {
+            id: 2,
+            rencontre: "Mouzillon/Les Herbiers",
+            poster: "EMouz_Herbiers.png",
+            fichier: "EMouz_Herbiers.mp4",
+            scenario: "EMouz_Herbiers.js",
+            variable: "script2",
+            description: "Match amical opposant Mouzillon aux Herbiers",
+            gauche: {
+                nom: "Les Herbiers",
+                fanion: "LHerbiers.png",
+                site: "https://www.vendeelesherbiersfootball.fr/pages/index.php",
+                maillotCouleur: "rgb(120, 83, 52)"
+            },
+            droite: {
+                nom: "Etoile Mouzillon",
+                fanion: "EMouz.png",
+                site: "https://etoile-mouzillon.footeo.com",
+                maillotCouleur: "rgb(255, 255, 255)"
+            },
+            arbitre: {
+                maillotCouleur: "rgb(94, 206, 226)"
+            }
+        }
+    ],
+    [
+        {
+            id: 3,
+            rencontre: "Nantes Metropole / Club Chavanoz",
+            poster: "Futsal_NMF_FCC.png",
+            fichier: "Futsal_NMF_FCC.mp4",
+            scenario: "Futsal_NMF_FCC.js",
+            variable: "script3",
+            description: "Match D1 Futsal opposant Nantes Métropole Futsal à Futsal Club Chavanoz",
+            gauche: {
+                nom: "Nantes Métropole Futsal",
+                fanion: "NMF.jpg",
+                site: "http://nantesmetropolefutsal.fr/",
+                maillotCouleur: "rgb(0,0,200)"
+            },
+            droite: {
+                nom: "Futsal Club Chavanoz",
+                fanion: "FCC.jpg",
+                site: "http://www.futsal-chavanoz.com/",
+                maillotCouleur: "rgb(255, 255, 255)"
+            },
+            arbitre: {
+                maillotCouleur: "rgb(0,200,0)"
+            }
+        }
+    ],
+    [
+        {
+            id: 4,
+            rencontre: "PDLL/Bretagne",
+            poster: "Pole_PloufraganMT2.png",
+            fichier: "Pole_PloufraganMT2.mp4",
+            scenario: "Pole_PloufraganMT2.js",
+            variable: "script4",
+            description: "Match amical opposant Les Pôles espoirs des Ligues de Bretagne (en blanc) et des Pays de La Loire (en bleu marine) - 2ème période",
+            gauche: {
+                nom: "Ligue Pays De la Loire",
+                fanion: "Ligue2.png",
+                site: "https://lfpl.fff.fr",
+                maillotCouleur: "rgb(67, 76, 108)"
+            },
+            droite: {
+                nom: "Ligue de Bretagne",
+                fanion: "ligueBretagne.png",
+                site: "https://footbretagne.fff.fr",
+                maillotCouleur: "rgb(255, 255, 255)"
+            },
+            arbitre: {
+                maillotCouleur: "rgb(255, 112, 119)"
+            }
+        }
+    ]
+]
